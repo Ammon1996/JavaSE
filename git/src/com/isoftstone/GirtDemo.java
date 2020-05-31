e672a657dad165d610824b318a9b8b0d17708404
M1ng-1996 <fymtticy@163.com>
2020-05-31 11:53:55 +0800

    本地工程添加到git

---
+++ git/src/com/isoftstone/GirtDemo.java
@@ -0,0 +1,13 @@
+package com.isoftstone;
+
+/**
+ * 描述:
+ *
+ * @author Ming
+ * @create 2020-05-31 11:47
+ */
+public class GirtDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
